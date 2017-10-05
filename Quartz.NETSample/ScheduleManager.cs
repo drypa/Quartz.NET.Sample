6a333f0ce906bb8dfbe69a2c3c0807fa6b5145d8
DRypa <dr.drypa.k@mail.ru>
2017-10-05 15:02:19 +0300

    crash second call of Shutdown() fix

--- Quartz.NETSample/ScheduleManager.cs
+++ Quartz.NETSample/ScheduleManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Quartz.Impl;
 
@@ -10,51 +11,84 @@ namespace Quartz.NETSample
         private bool _isEnabled = true;
         private IScheduler _scheduler;
         private DateTimeOffset _startDate = DateTimeOffset.UtcNow.AddYears(-20);
-
+        //private ITrigger _trigger;
+        private static JobKey jobKey = JobKey.Create("job");
+        private static TriggerKey triggerKey = new TriggerKey("trigger");
+        private IJobDetail _job = JobBuilder.Create<SimpleJob>().WithIdentity(jobKey).Build();
 
         public async Task Update(bool isEnabled, int intervalInSeconds, DateTimeOffset startDate)
         {
+            var needChangeEnabled = _isEnabled != isEnabled;
             _isEnabled = isEnabled;
+            var needChangeSchedule = _intervalInSeconds != intervalInSeconds || _startDate != startDate;
+
+            if (needChangeEnabled)
+            {
+                if (isEnabled)
+                {
+                    await _scheduler.ResumeJob(jobKey);
+                }
+                else
+                {
+                    await _scheduler.PauseJob(jobKey);
+                }
+            }
+            if (!isEnabled)
+            {
+                return;
+            }
             _intervalInSeconds = intervalInSeconds;
             _startDate = startDate;
-            await TriggerScheduler();
+            if (needChangeSchedule)
+            {
+                var trigger = await _scheduler.GetTrigger(triggerKey);
+                TriggerBuilder triggerBuilder = trigger.GetTriggerBuilder();
+                var newTrigger = CreateTrigger(triggerBuilder);
+
+                await _scheduler.RescheduleJob(triggerKey, newTrigger);
+            }
+
+        }
+
+        public ScheduleManager()
+        {
+            _scheduler = CreateScheduler().GetAwaiter().GetResult();
         }
 
         public async Task TriggerScheduler()
         {
-            var scheduler = await GetScheduler();
             if (_isEnabled)
             {
-                
-                await Console.Out.WriteLineAsync("Let's work!");
 
-                await scheduler.Start();
+                await Console.Out.WriteLineAsync("Let's work!");
+                await _scheduler.Start();
             }
             else
             {
                 await Console.Out.WriteLineAsync("Nothing to do!");
-                await scheduler.Shutdown();
+                await _scheduler.Shutdown();
             }
         }
 
-        private async Task<IScheduler> GetScheduler()
-        {
-            if (_scheduler == null)
-            {
-                var factory = new StdSchedulerFactory();
-                _scheduler = await factory.GetScheduler();
-            }
 
-            await _scheduler.Clear();
-            var job = JobBuilder.Create<SimpleJob>().Build();
-            var trigger = TriggerBuilder.Create()
+        private async Task<IScheduler> CreateScheduler()
+        {
+            var factory = new StdSchedulerFactory();
+            var scheduler = await factory.GetScheduler();
+            var trigger = CreateTrigger(TriggerBuilder.Create());
+
+            await scheduler.ScheduleJob(_job, trigger);
+            return scheduler;
+        }
+
+        private ITrigger CreateTrigger(TriggerBuilder triggerBuilder)
+        {
+            return triggerBuilder
                 .StartAt(_startDate)
+                .WithIdentity(triggerKey)
                 .WithSimpleSchedule(x => x.WithIntervalInSeconds(_intervalInSeconds)
                     .RepeatForever())
                 .Build();
-
-            await _scheduler.ScheduleJob(job, trigger);
-            return _scheduler;
         }
     }
 
